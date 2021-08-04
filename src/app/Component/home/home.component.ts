a4f1b3cbdd43199a1ccc3d65358e9e55e529ba3c
VihagaYohan <vihagayohan94@gmail.com>
2021-08-04 15:51:56 +0530

    Added basic card components to show total count in home screen

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+
+// service
+import { PostsService } from '../../Services/Posts/posts.service';
+import { UsersService } from '../../Services/Users/users.service';
+import { StoresService } from '../../Services/Stores/stores.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  postsData: any;
+  usersData: any;
+  storesData: any;
+  isLoading: boolean = true;
+  errorFound: boolean = false;
 
-  constructor() { }
+  constructor(
+    private postService: PostsService,
+    private userService: UsersService,
+    private storeService: StoresService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let posts = this.postService.GetAllPosts(
+      'https://localhost:5001/api/Posts/get-all-posts'
+    );
+    let users = this.userService.GetAllUsers(
+      'https://localhost:5001/api/Users/all-users'
+    );
+    let stores = this.storeService.getAllStores(
+      'https://localhost:5001/api/Stores/get-all-stores'
+    );
+
+    forkJoin([posts, users, stores]).subscribe(
+      (results) => {
+        this.postsData = results[0].data;
+        this.usersData = results[1].data;
+        this.storesData = results[2].data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+    /* this.httpService
+      .GetAllPosts('https://localhost:5001/api/Posts/get-all-posts')
+      .subscribe(
+        (response) => {
+         const {data} = response;
+         this.someData = data;
+          console.log(JSON.stringify(data));
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => console.log('done')
+      ); */
   }
-
 }
--- src/app/Services/Posts/posts.service.ts
+++ src/app/Services/Posts/posts.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  GetAllPosts(url:any){
+    return this.http.get<any>(url);
+  }
+  
 }
